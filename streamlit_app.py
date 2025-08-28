ad9ccc1a3ae2640bda61455ce667f1f80ff5328e
Usharani2903create <m.usharani901@gmail.com>
2025-08-28 18:02:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,17 @@ ingredients_list = st.multiselect(
 # Build ingredients string
 if ingredients_list:
     ingredients_string = ''
+    
+
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        
+        # Display nutrition info for each selected fruit
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    
+
 
     # Insert order into Snowflake
     my_insert_stmt = f"""INSERT INTO smoothies.public.orders(ingredients, name_on_order)
